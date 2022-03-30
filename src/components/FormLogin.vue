d4834f50a36633f4381dd9811ab430549adb884b
Phangster <mynamebryanphang@gmail.com>
2022-03-30 18:49:17 +0800

    init repo

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="my-2">
+      <div>Username</div>
+      <input
+        v-model="form.username"
+        id="username"
+        placeholder="Username"
+      />
+    </div>
+    <div class="my-2">
+      <div>Password</div>
+      <input
+        v-model="form.password"
+        id="password"
+        placeholder="Password"
+      />
+    </div>
+    <div class="text-danger my-2">{{ userStore.state.error }}</div>
+    <button id="login" @click="onSubmit" class="btn btn-success btn-block my-2">
+      Login
+    </button>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+import userStore from '@/stores/user';
+
+export default defineComponent({
+  setup() {
+    const form = reactive({
+      username: '',
+      password: '',
+    });
+
+    const onSubmit = () => {
+      userStore.login(form.username, form.password);
+      form.username = '';
+      form.password = '';
+    };
+
+    return { form, userStore, onSubmit };
+  },
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div v-if="!userStore.getters.isLoggedIn" class="d-grid gap-2 col-6 mx-auto">
+    <FormLogin />
+  </div>
+  <div v-else class="text-center">
+    <h2 id="title">Welcome Page, {{ userStore.state.name }}</h2>
+    <div>
+      <a href="https://www.wikipedia.org/">Wikipedia link</a>
+    </div>
+    <div class="qrcode">
+      <img id="reader" class="my-image" :src="`data:image/png;base64,${testQR}`" alt="test"/>
+    </div>
+    <div>
+      <iframe
+        id="iframe"
+        title="This is a unique title"
+        src="sample.html"
+      />
+    </div>
+    <div>
+      <button id="file_download" class="btn btn-secondary" @click="download">
+        download
+      </button>
+    </div>
+    <button id="logout" class="btn btn-secondary" @click="logout">
+      Logout
+    </button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import {
+  defineComponent, onMounted, computed, ref,
+} from 'vue';
+import userStore from '@/stores/user';
+import FormLogin from '@/components/FormLogin.vue';
+import FileSaver from 'file-saver';
+import { imgSrcToBlob, blobToBase64String } from 'blob-util';
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+  name: 'App',
+  components: { FormLogin },
+  setup() {
+    // eslint-disable-next-line global-require
+    const qrImage = computed(() => require('@/assets/test_qr.png'));
+    const testQR = ref();
+    onMounted(() => {
+      // eslint-disable-next-line no-unused-expressions
+      userStore.getUser;
+      imgSrcToBlob(qrImage.value).then((blob) => {
+        blobToBase64String(blob).then((base64String) => {
+          testQR.value = base64String;
+        }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+
+    const logout = () => {
+      userStore.logout();
+      console.log('logging out');
+      console.log(userStore.state);
+    };
+
+    const download = () => {
+      console.log('downloading');
+      const blob = new Blob(['Hello, world!'], { type: 'text/plain;charset=utf-8' });
+      FileSaver.saveAs(blob, 'hello world.txt');
+    };
+    return {
+      userStore, logout, download, qrImage, testQR,
+    };
   },
 });
 </script>
+<style lang="css" scoped>
+.my-image {
+  width: 200px;
+  height: 200px;
+}
+</style>
